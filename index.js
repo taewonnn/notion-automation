5df3dcced151b0cd26b922e33bab54cafe34108f
eden <eden@rainbow.co.kr>
2025-01-08 18:31:37 +0900

    test: console

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { Client } = require('@notionhq/client');
 
 // Notion 클라이언트 초기화
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
-
+// https://www.notion.so/rainbow8/1435f6cbe2fd80c6b09feaf0467a6287?v=1435f6cbe2fd81b7b79f000c223cdb02&pvs=4
+// https://www.notion.so/rainbow8/f4cb0f3df60f47d8aee73e82fea59b84?v=af5fb884bb2644798e71a70129e98437&pvs=4
 const sourceDbId = '1435f6cbe2fd80c6b09feaf0467a6287';
 const targetDbId = 'f4cb0f3df60f47d8aee73e82fea59b84';
 
@@ -20,14 +21,16 @@ async function moveOldDoneCards() {
     database_id: sourceDbId,
     filter: {
       and: [
-        { property: '상태', select: { equals: 'DONE' } },
+        { property: 'Status', status: { equals: 'Done' } },
         {
-          property: '완료 날짜',
+          property: 'Date',
           date: { before: sevenDaysAgo.toISOString() },
         },
       ],
     },
   });
+
+  // console.log('!!',response);
 
   for (const page of response.results) {
     await notion.pages.create({
